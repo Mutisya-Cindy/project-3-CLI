91c0da3d57163b43f4b2fbde2f3ae259e4571ed4
Cindy <cindy.mbishmutisya@gmail.com>
2024-06-15 22:07:49 +0300

    helpers updated

--- helpers.py
+++ helpers.py
@@ -16,3 +16,18 @@ def list_bookings():
     bookings = Booking.all()
     for booking in bookings:
         print(f"Booking ID: {booking['id']}, Customer ID: {booking['customer_id']}, Plumber ID: {booking['plumber_id']}, Date: {booking['date']}, Time: {booking['time']}")
+
+def add_customer():
+    name = input("Name: ")
+    email = input("Email: ")
+    customer = Customer(name, email)
+    customer.save()
+    print(f"Customer {customer.name} added")       
+
+def add_plumber():
+    name = input("Name: ")
+    rate = float(input("Rate: "))
+    plumber = Plumber(name, rate)
+    plumber.save()
+    print(f"Plumber {plumber.name} added")
+
